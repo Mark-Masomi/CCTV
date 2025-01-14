8df4b6be8159e2b8be973a0b4e9ee5364a15bfea
mark-masomi <moinmirmasomi@outlook.com>
2025-01-14 19:49:58 +0100

    Added neccesery classes!

--- src/main/java/org/example/CameraManager.java
+++ src/main/java/org/example/CameraManager.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class AudioManager {
+public class CameraManager {
+
+
+    /*private VideoCapture camera;
+    public void startRecording() {
+        camera = new VideoCapture("rtsp://your_camera_ip");
+        if (!camera.isOpened()) {
+            System.out.println("Camera connection failed");
+            return;
+        } */
+
 }
--- src/main/java/org/example/LocationManager.java
+++ src/main/java/org/example/LocationManager.java
@@ -1,4 +1,4 @@
 package org.example;
 
-public class AudioManager {
+public class LocationManager {
 }
