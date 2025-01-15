498623b639d3b0a8274f5d1d5c5b3d2688ffdc0f
mark-masomi <moinmirmasomi@outlook.com>
2025-01-15 21:41:48 +0100

    got to save the users geolocations saved but its not precise enough,trying to get the info from pcs gps,in order to be able get the  location stamping in place

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    //implementation ("org.opencv:opencv:4.5.1")
     implementation (files("libs/opencv-4100.jar"))
-   }
+    implementation (files("libs/javagps.jar"))
+    implementation("io.ipinfo:ipinfo-api:3.0.0")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.1")
+    implementation ("org.json:json:20210307")
+}
 
 tasks.test {
     useJUnitPlatform()
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,74 @@
 package org.example;
 
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+
+        try{
+
+            // Create an HttpClient instance
+            HttpClient client = HttpClient.newHttpClient();
+
+            // Create the API request to ipinfo.io
+            HttpRequest requeast= HttpRequest.newBuilder()
+                    .uri(new URI("http://ipinfo.io/json")).build();
+
+            //send request and get response
+            HttpResponse<String> response =client.send(requeast,HttpResponse.BodyHandlers.ofString());
+
+
+            JSONObject json=new JSONObject(response.body());
+
+            String ip=json.getString("ip");
+            String city=json.getString("city");
+            String region=json.getString("region");
+            String country=json.getString("country");
+            String location=json.getString("loc");
+
+            System.out.println("IP Address: " + ip);
+            System.out.println("City: " + city);
+            System.out.println("Region: " + region);
+            System.out.println("Country: " + country);
+            System.out.println("Location (lat, long): " + location);
+
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+        /*try {
+            // Create a GPS driver instance
+            GPSDriver gpsDriver = new GPSDriver();
+
+            // Add the listener to the driver
+            gpsDriver.addGPSListener(new GPSListener() {
+                @Override
+                public void gpsEvent(GPSInfo gpsInfo) {
+
+                }
+
+                // Ensure method signature matches the interface
+                public void gpsData(GPSInfo data) {
+                    // Assuming GPSInfo has methods getLatitude() and getLongitude()
+                    System.out.println("Latitude: " + data.latitude);
+                    System.out.println("Longitude: " + data.longitude);
+                }
+            });
+
+            // Start listening for GPS data
+            gpsDriver.run();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+        }
+
+
+
 }
